289593bfb9420e4c852ef4c6c502e65660875016
Stephen Bash <scbash@gmail.com>
2015-02-22 17:05:25 -0500

    Initial commit for TalkingNotifications based on SMSReader tutorial
    http://code.tutsplus.com/tutorials/use-text-to-speech-on-android-to-read-out-incoming-messages--cms-22524

---
+++ app/src/main/java/com/crystalnebula/talkingnotifications/Speaker.java
@@ -0,0 +1,72 @@
+package com.crystalnebula.talkingnotifications;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.speech.tts.TextToSpeech;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * Created by scbash on 2/22/15.
+ */
+public class Speaker implements TextToSpeech.OnInitListener
+{
+    private TextToSpeech tts;
+
+    private boolean ready = false;
+
+    private boolean allowed = false;
+
+    public Speaker(Context context)
+    {
+        tts = new TextToSpeech(context, this);
+    }
+
+    public boolean isAllowed()
+    {
+        return allowed;
+    }
+
+    public void allow(boolean allowed)
+    {
+        this.allowed = allowed;
+    }
+
+    @Override
+    public void onInit(int status)
+    {
+        if (status == TextToSpeech.SUCCESS)
+        {
+           // Change this to match your locale
+           tts.setLanguage(Locale.US);
+           ready = true;
+        }
+        else
+        {
+            ready = false;
+        }
+    }
+
+    public void speak(String text)
+    {
+        // Speak only if TTS is ready and the user has allowed speach
+        if (ready && allowed)
+        {
+            HashMap<String, String> hash = new HashMap<String, String>();
+            hash.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
+                     String.valueOf(AudioManager.STREAM_NOTIFICATION));
+            tts.speak(text, TextToSpeech.QUEUE_ADD, hash);  // TODO: speak is deprecated!
+        }
+    }
+
+    public void pause(int duration)
+    {
+        tts.playSilence(duration, TextToSpeech.QUEUE_ADD, null); // TODO: playSilence is also deprecated!
+    }
+
+    public void destroy()
+    {
+        tts.shutdown();
+    }
+}
