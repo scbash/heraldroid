1e0fcf4e67b057d915bef03544e9ac6674fdcac4
Stephen Bash <scbash@gmail.com>
2015-02-22 17:56:11 -0500

    Clean up warnings in Speaker class based on Android Studio recommendations

--- app/src/main/java/com/crystalnebula/talkingnotifications/Speaker.java
+++ app/src/main/java/com/crystalnebula/talkingnotifications/Speaker.java
@@ -8,6 +8,9 @@ import java.util.HashMap;
 import java.util.Locale;
 
 /**
+ * Quoting the original tutorial, "... a helper class for the TTS engine.  This class is used to
+ * avoid calling the TTS API directly from the Activity."
+ *
  * Created by scbash on 2/22/15.
  */
 public class Speaker implements TextToSpeech.OnInitListener
@@ -23,6 +26,7 @@ public class Speaker implements TextToSpeech.OnInitListener
         tts = new TextToSpeech(context, this);
     }
 
+    @SuppressWarnings("unused")  // it's just good style to have getters for private members
     public boolean isAllowed()
     {
         return allowed;
@@ -48,21 +52,31 @@ public class Speaker implements TextToSpeech.OnInitListener
         }
     }
 
+    /* This form of speak is deprecated in API 21, but it's replacement is also introduced in API
+     * 21.  So for backwards compatibility with API 18 (desired functional API level for this
+     * project), use the old method and suppress the warning.
+     */
+    @SuppressWarnings("deprecation")
     public void speak(String text)
     {
-        // Speak only if TTS is ready and the user has allowed speach
+        // Speak only if TTS is ready and the user has allowed speech
         if (ready && allowed)
         {
-            HashMap<String, String> hash = new HashMap<String, String>();
+            HashMap<String, String> hash = new HashMap<>();
             hash.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
                      String.valueOf(AudioManager.STREAM_NOTIFICATION));
-            tts.speak(text, TextToSpeech.QUEUE_ADD, hash);  // TODO: speak is deprecated!
+            tts.speak(text, TextToSpeech.QUEUE_ADD, hash);
         }
     }
 
+    /* This form of playSilence is deprecated in API 21, but it's replacement is also introduced
+     * in API 21.  So for backwards compatibility with API 18 (desired functional API level for
+     * this project), use the old method and suppress the warning.
+     */
+    @SuppressWarnings("deprecation")
     public void pause(int duration)
     {
-        tts.playSilence(duration, TextToSpeech.QUEUE_ADD, null); // TODO: playSilence is also deprecated!
+        tts.playSilence(duration, TextToSpeech.QUEUE_ADD, null);
     }
 
     public void destroy()
